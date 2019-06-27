4bed4664d2d957e497f93e5237bc5a25906b15f0
liuzhiwei <2606212687@qq.com>
2019-06-27 17:25:56 +0800

    2

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -8,7 +8,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Item {
+	//id
 	private Integer id;
+	//name
 	private String name;
 	private Integer number;
 }
